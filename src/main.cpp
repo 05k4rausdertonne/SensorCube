8a248c433811b45ff23a464cdf034c55b69faa30
05k4rausdertonne <oskarflath@gmail.com>
2021-11-18 14:57:48 +0100

    fix compiling issues

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,14 @@
 // http interface libraries
 #include <ESP8266WebServer.h>
 
+// handling serialisation and deserialization of json documents
+#include <ArduinoJson.h>
+
+// c++ core libraries
+#include <map>
+#include <vector>
+#include <iterator>
+
 // low level interface libraries
 #include <SPI.h>
 #include <Wire.h>
@@ -16,16 +24,11 @@
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
 
-// c++ core libraries
-#include <vector>
-#include <map>
-
-// library for the conversion of json documents
-#include <ArduinoJson.h>
-
-// define connection pins
+// define spi connection pins
 #define RST_PIN         D0 
 #define SS_PIN          D8 
+
+// TODO C++ ist müll, der scheiß muss in einen file -_-
 
 // init Wifi
 WiFiClient wifiClient;
@@ -33,13 +36,14 @@ WiFiClient wifiClient;
 // init web server
 ESP8266WebServer  server(80);
 
-// init sensor objects etc.
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
-Adafruit_BME680 bme; // I2C configuration
-unsigned long bmeScan = 0;
-
+// init mqtt client
 PubSubClient mqttClient(wifiClient);
-char* serverAddressMQTT = "broker.hivemq.com";
+
+// Create MFRC522 instance
+MFRC522 mfrc522(SS_PIN, RST_PIN);  
+
+// Create BME680 instance in I2C configuration
+Adafruit_BME680 bme; 
 
 // init global args with default parameters
 std::map<String, String> globalArgs = {
@@ -52,101 +56,196 @@ std::map<String, String> globalArgs = {
     {"sealevelpressure", "1013.25"}
 };
 
-
-struct sensorChip
-{
-    bool async;
-    unsigned long scanEnd = 0;
-    bool scanning = false;
-    uint32_t (*startAsyncReading)();
-};
-
-sensorChip makeSensorChip(
-    bool async,
-    uint32_t (*startAsyncReading)()
-    )
-{
-    if (async)
-    {
-        return {
-        async,
-        0,
-        false,
-        startAsyncReading,
-        };
-    } 
-    else return {false};
-}
-
-sensorChip makeSensorChip(
-    bool async
-)    
-{
-    return {false};
-}
+//--------------------
+//
+// sensor utils
+//
+//--------------------
 
 struct sensor
 {
     unsigned long lastScanned;
     unsigned int scanInterval;
-    sensorChip* chip;
     String (*readSensor)(String type);
+    String lastValue;
+    unsigned long lastNewValue;
+    unsigned int bounceTime;
 };
 
-sensor makeSensor(
-    unsigned int scanInterval,
-    sensorChip* chip,
-    String (*readSensor)(String type)
-    )
+// bme680 functions
+void initBME680()
 {
-    return {
-        0,
-        scanInterval,
-        chip,
-        readSensor
-    };
-}
-
-// callback function for reading RFID tags
-String readMFRC522(String type)
-{
-    String value = "";
-
-    if (mfrc522.PICC_IsNewCardPresent())  // (true, if RFID tag/card is present ) PICC = Proximity Integrated Circuit Card
-    {
-        StaticJsonDocument<48> doc;
-
-        if(mfrc522.PICC_ReadCardSerial()) { // true, if RFID tag/card was read
-            
-            for (byte i = 0; i < mfrc522.uid.size; ++i) { // read id (in parts)
-            
-            value.concat(String(mfrc522.uid.uidByte[i], HEX)); // print id as hex values
-            }
-        }
+    if (!bme.begin()) {
+    Serial.println("Could not find a valid BME680 sensor, check wiring!");
     }
-    return value;
+
+    // Set up bme680 oversampling and filter initialization
+    bme.setTemperatureOversampling(BME680_OS_8X);
+    bme.setHumidityOversampling(BME680_OS_2X);
+    bme.setPressureOversampling(BME680_OS_4X);
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    bme.setGasHeater(320, 150); // 320*C for 150 ms
 }
 
 //callback function for collecting readings from bme680
 String readBME680(String type)
 {   
+    // make synchronous reading needs to be changed to async if that takes to long
+    // mb make all readings async?
+    if (! bme.performReading()) 
+    {
+        Serial.println("Failed to perform reading :(");
+        return "";
+    }
+
     String value; 
 
     if(type ==  "temperature") value = (String)bme.temperature;
     else if(type ==  "pressure") value = (String)bme.pressure;
     else if(type ==  "humidity") value = (String)bme.humidity;
     else if(type ==  "gas_resistance") value = (String)bme.gas_resistance;
-    else if(type ==  "altitude") value = (String)bme.readAltitude(globalArgs.at("sealevelpressure").toFloat());
     
     return value;
 }
 
-uint32_t startBME680()
+// MFRC522 functions
+void initMFRC522()
 {
-    return bme.beginReading();
+    // init RFID shield
+    SPI.begin();			// Init SPI bus
+    mfrc522.PCD_Init();		// Init MFRC522
+
+    delay(5);
+
+    Serial.print("RFID tag reader ");
+    mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
+
 }
 
+String readMFRC522(String type)
+{
+
+    String value = "";
+
+    if(type == "nfcID")
+    {
+        if (mfrc522.PICC_IsNewCardPresent())  // (true, if RFID tag/card is present ) PICC = Proximity Integrated Circuit Card
+        {
+
+            if(mfrc522.PICC_ReadCardSerial()) { // true, if RFID tag/card was read
+                
+                for (byte i = 0; i < mfrc522.uid.size; ++i) { // read id (in parts)
+                
+                value.concat(String(mfrc522.uid.uidByte[i], HEX)); // print id as hex values
+                }
+            }
+        }
+    } 
+
+    return value;
+}
+
+// factory method for sensor struct
+sensor makeSensor(
+    unsigned int scanInterval,
+    String (*readSensor)(String type),
+    unsigned int bounceTime
+    )
+{
+    return {
+        0,
+        scanInterval,
+        readSensor,
+        "",
+        0,
+        bounceTime
+    };
+}
+
+// vector for storing sensors
+std::map<String, sensor> sensors =
+{
+    {"temperature", makeSensor(10000, readBME680, 0)},
+    {"pressure", makeSensor(10000, readBME680, 0)},
+    {"humidity", makeSensor(10000, readBME680, 0)},
+    {"gas_resistance", makeSensor(10000, readBME680, 0)},
+    {"altitude", makeSensor(10000, readBME680, 0)},
+    {"nfcID", makeSensor(100, readMFRC522, 5000)}
+};
+
+// init function for the sensors
+void initSensors()
+{
+    // call individual sensor inits here
+    initBME680();
+    initMFRC522();
+}
+
+
+
+// loop function for the sensors
+void loopSensors()
+{
+    for (std::map<String, sensor>::iterator it = sensors.begin(); it != sensors.end(); it++)
+    {
+        if(millis() - it->second.lastScanned > it->second.scanInterval)
+        {
+            String value = it->second.readSensor(it->first);
+            
+            if(value != it->second.lastValue
+                && millis() - it->second.lastNewValue > it->second.bounceTime)
+            {
+                publishSensor(value, it->first);
+
+                it->second.lastValue = value;
+                it->second.lastNewValue = millis();
+
+            }
+                
+            it->second.lastScanned = millis();
+        }
+    }
+}
+
+// ---------------------------
+//
+// actuator utils
+//
+// ---------------------------
+
+// struct actuatorValue
+// {
+//     String name;
+//     int current;
+//     int target;
+//     +
+// }
+
+// struct actuator
+// {
+//     std::map<String, int>;
+//     String (*writeActuator)(String type);
+//     String lastValue;
+//     unsigned long lastNewValue;
+//     unsigned int bounceTime;
+// };
+
+// void initActuators()
+// {
+
+// }
+
+// void loopActuators()
+// {
+
+// }
+
+
+// ---------------------------
+//
 // MQTT client helper functions
+//
+// ---------------------------
 void reconnectMQTT()
 {   
     Serial.println("attempting reconnect to MQTT broker..");
@@ -161,6 +260,14 @@ void reconnectMQTT()
     {
         Serial.println("reconnect failed :(");
     }
+}
+
+void setMQTTServer()
+{
+    mqttClient.disconnect();
+
+    mqttClient.setServer(globalArgs.find("address")->second.c_str(), 1883);
+                
 }
 
 void handleMessageMQTT(char* topic, byte* payload, unsigned int length)
@@ -182,13 +289,6 @@ bool publishSensor(String payload, String sensorType)
         return false;
     }
 
-    if (!mqttClient.connected())
-    {
-        reconnectMQTT();
-    }
-
-    
-
     String outTopic = "sensor/";
     outTopic.concat(sensorType + "/");
     outTopic.concat(globalArgs.find("uuid")->second + "/");
@@ -202,28 +302,17 @@ bool publishSensor(String payload, String sensorType)
     }
     else   
     {
-        Serial.println("publish failed");
+        Serial.print("publish failed: ");
+        Serial.println(sensorType);
         return false;
     }
 }
 
-
-std::map<String, sensorChip> sensorChips =
-{
-    {"bme680", makeSensorChip(true, startBME680)},
-    {"mfrc522", makeSensorChip(false)}
-};
-
-// vector for storing sensors
-std::map<String, sensor> sensors =
-{
-    {"temperature", makeSensor(10000,&(sensorChips.at("bme680")), readBME680)},
-    {"pressure", makeSensor(10000,&(sensorChips.at("bme680")), readBME680)},
-    {"humidity", makeSensor(10000,&(sensorChips.at("bme680")), readBME680)},
-    {"gas_resistance", makeSensor(10000,&(sensorChips.at("bme680")), readBME680)},
-    {"altitude", makeSensor(10000,&(sensorChips.at("bme680")), readBME680)},
-    {"nfcID", makeSensor(500,&(sensorChips.at("mfrc522")), readMFRC522)}
-};
+// ---------------------------
+//
+// REST interface helper functions
+//
+// ---------------------------
 
 String serializeConfig()
 {
@@ -279,11 +368,7 @@ void handleRootRequest()
                 
                 globalArgs.at(it->first) = (String)doc[it->first];
 
-                mqttClient.disconnect();
-
-                mqttClient.setServer(globalArgs.find("address")->second.c_str(), 1883);
-                
-                // reconnectMQTT();
+                setMQTTServer();
                 
                 Serial.println(it->second);                
             }
@@ -304,7 +389,9 @@ void handleSensorRequest()
         StaticJsonDocument<128> doc;
         deserializeJson(doc, server.arg("plain"));
         
-        for (std::map<String, sensor>::iterator it = sensors.begin(); it != sensors.end(); it++)
+        for (std::map<String, sensor>::iterator it = sensors.begin();
+            it != sensors.end(); 
+            it++)
         {
             if (doc.containsKey(it->first) 
                 && doc[it->first].containsKey("scanInterval"))
@@ -323,23 +410,12 @@ void handleNotFound()
     server.send(404, "text/plain", "not found. go somewhere else.");
 }
 
-// ---------------------------
-//
-// main setup function
-//
-// ---------------------------
-void setup()
+
+
+//-------------------------------------------
+
+void initComms()
 {
-
-    // globalArgs.emplace("location", "tbd");
-    
-    Serial.begin(115200);
-    while (!Serial);
-
-    if (!bme.begin()) {
-    Serial.println("Could not find a valid BME680 sensor, check wiring!");
-    }
-
     //init WiFi connection
     WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
     
@@ -352,29 +428,44 @@ void setup()
 
     // configure mqtt connection
     mqttClient.setCallback(handleMessageMQTT);
-    mqttClient.setServer(globalArgs.find("address")->second.c_str(), 1883);
-
-    // init RFID shield
-    SPI.begin();			// Init SPI bus
-    mfrc522.PCD_Init();		// Init MFRC522
-
-    delay(5);
-
-    Serial.print("RFID tag reader ");
-    mfrc522.PCD_DumpVersionToSerial();	// Show details of PCD - MFRC522 Card Reader details
-
-    // Set up bme680 oversampling and filter initialization
-    bme.setTemperatureOversampling(BME680_OS_8X);
-    bme.setHumidityOversampling(BME680_OS_2X);
-    bme.setPressureOversampling(BME680_OS_4X);
-    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-    bme.setGasHeater(320, 150); // 320*C for 150 ms
+    mqttClient.setServer(
+        globalArgs.find("address")->second.c_str(), 
+        globalArgs.find("port")->second.toInt());
 
     server.on("/", handleRootRequest);
     server.on("/sensor", handleSensorRequest);
     server.onNotFound(handleNotFound);
 
     server.begin();
+}
+
+void loopComms()
+{
+    server.handleClient();
+    // handle MQTT client
+    if (!mqttClient.connected())
+    {
+        reconnectMQTT();
+        return;
+    }
+    mqttClient.loop();
+}
+
+// ---------------------------
+//
+// main setup function
+//
+// ---------------------------
+void setup()
+{    
+    Serial.begin(115200);
+    while (!Serial);
+
+    initComms();
+
+    initSensors();
+
+    initActuators();
 
     delay(50);
 }
@@ -386,48 +477,11 @@ void setup()
 // ---------------------------
 void loop()
 {   
-    server.handleClient();
-    // handle MQTT client
-    if (!mqttClient.connected())
-    {
-        reconnectMQTT();
-        return;
-    }
-    mqttClient.loop();
+    loopComms();
 
-    if(millis() > bmeScan)
-    {
-        bmeScan = bme.beginReading();
-        if (bmeScan == 0) 
-        {
-            Serial.println(F("Failed to begin reading :("));
-        }
-    }
+    loopSensors();
 
-    // loop over sensors and check if any need to be read, 
-    // or async operations need to be started or finished
-
-    for (std::map<String, sensorChip>::iterator it = sensorChips.begin(); it != sensorChips.end(); it++)
-    {
-        if(it->second.async && millis() > it->second.scanEnd)
-        {
-            it->second.scanEnd = it->second.startAsyncReading();
-            if (it->second.scanEnd == 0) 
-            {
-                Serial.println(F("Failed to begin reading :("));
-            }
-        }
-    }
-
-    for (std::map<String, sensor>::iterator it = sensors.begin(); it != sensors.end(); it++)
-    {
-        if(millis() - it->second.lastScanned > it->second.scanInterval)
-        {
-            publishSensor(it->second.readSensor(it->first), it->first);
-                
-            it->second.lastScanned = millis();
-        }
-    }
+    loopActuators();
 }
 
 
